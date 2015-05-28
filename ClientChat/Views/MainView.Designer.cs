446a33fb33d43ca6ec9ed0469dfd67192d2f8547
luanamartins <lms7@cin.ufpe.br>
2015-05-27 23:08:42 -0300

    Basic layout of Login window

--- ClientChat/Views/MainView.Designer.cs
+++ ClientChat/Views/MainView.Designer.cs
@@ -28,35 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.displayMessageBox = new System.Windows.Forms.TextBox();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.sendButton = new System.Windows.Forms.Button();
+            this.userMessageBox = new System.Windows.Forms.TextBox();
             this.cancelButton = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.flowLayoutPanel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
-            // 
-            this.panel1.Location = new System.Drawing.Point(2, 3);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(154, 289);
-            this.panel1.TabIndex = 0;
-            // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.Controls.Add(this.textBox2);
+            this.flowLayoutPanel1.Controls.Add(this.displayMessageBox);
             this.flowLayoutPanel1.Location = new System.Drawing.Point(162, 3);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(375, 170);
             this.flowLayoutPanel1.TabIndex = 1;
             // 
+            // displayMessageBox
+            // 
+            this.displayMessageBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.displayMessageBox.Location = new System.Drawing.Point(3, 3);
+            this.displayMessageBox.Multiline = true;
+            this.displayMessageBox.Name = "displayMessageBox";
+            this.displayMessageBox.ReadOnly = true;
+            this.displayMessageBox.Size = new System.Drawing.Size(365, 167);
+            this.displayMessageBox.TabIndex = 0;
+            // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.userMessageBox);
             this.panel2.Controls.Add(this.cancelButton);
             this.panel2.Controls.Add(this.sendButton);
             this.panel2.Location = new System.Drawing.Point(162, 179);
@@ -64,14 +67,13 @@
             this.panel2.Size = new System.Drawing.Size(375, 113);
             this.panel2.TabIndex = 2;
             // 
-            // sendButton
+            // userMessageBox
             // 
-            this.sendButton.Location = new System.Drawing.Point(288, 13);
-            this.sendButton.Name = "sendButton";
-            this.sendButton.Size = new System.Drawing.Size(87, 40);
-            this.sendButton.TabIndex = 0;
-            this.sendButton.Text = "Send";
-            this.sendButton.UseVisualStyleBackColor = true;
+            this.userMessageBox.Location = new System.Drawing.Point(3, 13);
+            this.userMessageBox.Multiline = true;
+            this.userMessageBox.Name = "userMessageBox";
+            this.userMessageBox.Size = new System.Drawing.Size(279, 97);
+            this.userMessageBox.TabIndex = 0;
             // 
             // cancelButton
             // 
@@ -81,31 +83,45 @@
             this.cancelButton.TabIndex = 1;
             this.cancelButton.Text = "Cancel";
             this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
             // 
-            // textBox1
+            // sendButton
             // 
-            this.textBox1.Location = new System.Drawing.Point(16, 13);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(266, 81);
-            this.textBox1.TabIndex = 0;
+            this.sendButton.Location = new System.Drawing.Point(288, 13);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(87, 40);
+            this.sendButton.TabIndex = 0;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
             // 
-            // textBox2
+            // tableLayoutPanel1
             // 
-            this.textBox2.Location = new System.Drawing.Point(3, 3);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(365, 167);
-            this.textBox2.TabIndex = 0;
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.White;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 8;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 48.61111F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 51.38889F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(144, 286);
+            this.tableLayoutPanel1.TabIndex = 3;
             // 
             // MainView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(542, 304);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.flowLayoutPanel1);
-            this.Controls.Add(this.panel1);
             this.Name = "MainView";
             this.Text = "MainView";
             this.Load += new System.EventHandler(this.MainView_Load);
@@ -119,12 +135,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button cancelButton;
         private System.Windows.Forms.Button sendButton;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox userMessageBox;
+        private System.Windows.Forms.TextBox displayMessageBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
