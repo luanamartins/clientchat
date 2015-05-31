975ae69c7d1cad9df2766a57a1362a4e1a07c1d6
luanamartins <lms7@cin.ufpe.br>
2015-05-31 13:02:29 -0300

    Fix New user account window - Change in the class name: NewUserAccountView to NewAccountView - Class was not loading

--- ClientChat/Views/NewAccountView.cs
+++ ClientChat/Views/NewAccountView.cs
@@ -9,16 +9,16 @@ using System.Windows.Forms;
 
 namespace ClientChat.Views
 {
-    public partial class NewUserAccountView : Form
+    public partial class NewAccountView : Form
     {
-        public NewUserAccountView()
+        public NewAccountView()
         {
             InitializeComponent();
         }
 
         private void buttonSend_Click(object sender, EventArgs e)
         {
-            // TODO Create new user
+            // TODO Send new user information for server
         }
 
         private void buttonCancel_Click(object sender, EventArgs e)
