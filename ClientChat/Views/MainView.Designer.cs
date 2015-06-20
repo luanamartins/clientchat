a836779f8ed4a53bb0416f0e11faf0b1fae8f89f
luanamartins <lms7@cin.ufpe.br>
2015-06-20 10:08:48 -0300

    Add radiobuttons for http and https connections.

--- ClientChat/Views/MainView.Designer.cs
+++ ClientChat/Views/MainView.Designer.cs
@@ -35,6 +35,8 @@
             this.cancelButton = new System.Windows.Forms.Button();
             this.sendButton = new System.Windows.Forms.Button();
             this.tableUserFriendsLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.radioButtonHttp = new System.Windows.Forms.RadioButton();
+            this.radioButtonHttps = new System.Windows.Forms.RadioButton();
             this.flowLayoutPanel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -59,17 +61,19 @@
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.radioButtonHttps);
+            this.panel2.Controls.Add(this.radioButtonHttp);
             this.panel2.Controls.Add(this.userMessageBox);
             this.panel2.Controls.Add(this.cancelButton);
             this.panel2.Controls.Add(this.sendButton);
             this.panel2.Location = new System.Drawing.Point(162, 179);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(375, 113);
+            this.panel2.Size = new System.Drawing.Size(375, 132);
             this.panel2.TabIndex = 2;
             // 
             // userMessageBox
             // 
-            this.userMessageBox.Location = new System.Drawing.Point(3, 13);
+            this.userMessageBox.Location = new System.Drawing.Point(3, 32);
             this.userMessageBox.Multiline = true;
             this.userMessageBox.Name = "userMessageBox";
             this.userMessageBox.Size = new System.Drawing.Size(279, 97);
@@ -77,9 +81,9 @@
             // 
             // cancelButton
             // 
-            this.cancelButton.Location = new System.Drawing.Point(288, 59);
+            this.cancelButton.Location = new System.Drawing.Point(288, 83);
             this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(87, 35);
+            this.cancelButton.Size = new System.Drawing.Size(87, 46);
             this.cancelButton.TabIndex = 1;
             this.cancelButton.Text = "Cancel";
             this.cancelButton.UseVisualStyleBackColor = true;
@@ -87,9 +91,9 @@
             // 
             // sendButton
             // 
-            this.sendButton.Location = new System.Drawing.Point(288, 13);
+            this.sendButton.Location = new System.Drawing.Point(288, 32);
             this.sendButton.Name = "sendButton";
-            this.sendButton.Size = new System.Drawing.Size(87, 40);
+            this.sendButton.Size = new System.Drawing.Size(87, 45);
             this.sendButton.TabIndex = 0;
             this.sendButton.Text = "Send";
             this.sendButton.UseVisualStyleBackColor = true;
@@ -114,11 +118,33 @@
             this.tableUserFriendsLayout.Size = new System.Drawing.Size(144, 286);
             this.tableUserFriendsLayout.TabIndex = 3;
             // 
+            // radioButtonHttp
+            // 
+            this.radioButtonHttp.AutoSize = true;
+            this.radioButtonHttp.Location = new System.Drawing.Point(4, 9);
+            this.radioButtonHttp.Name = "radioButtonHttp";
+            this.radioButtonHttp.Size = new System.Drawing.Size(54, 17);
+            this.radioButtonHttp.TabIndex = 2;
+            this.radioButtonHttp.TabStop = true;
+            this.radioButtonHttp.Text = "HTTP";
+            this.radioButtonHttp.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonHttps
+            // 
+            this.radioButtonHttps.AutoSize = true;
+            this.radioButtonHttps.Location = new System.Drawing.Point(96, 9);
+            this.radioButtonHttps.Name = "radioButtonHttps";
+            this.radioButtonHttps.Size = new System.Drawing.Size(61, 17);
+            this.radioButtonHttps.TabIndex = 3;
+            this.radioButtonHttps.TabStop = true;
+            this.radioButtonHttps.Text = "HTTPS";
+            this.radioButtonHttps.UseVisualStyleBackColor = true;
+            // 
             // MainView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(542, 304);
+            this.ClientSize = new System.Drawing.Size(543, 323);
             this.Controls.Add(this.tableUserFriendsLayout);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.flowLayoutPanel1);
@@ -142,5 +168,7 @@
         private System.Windows.Forms.TextBox userMessageBox;
         private System.Windows.Forms.TextBox displayMessageBox;
         private System.Windows.Forms.TableLayoutPanel tableUserFriendsLayout;
+        private System.Windows.Forms.RadioButton radioButtonHttps;
+        private System.Windows.Forms.RadioButton radioButtonHttp;
     }
 }
