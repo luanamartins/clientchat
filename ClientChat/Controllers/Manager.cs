2922723298f4d311315b192f4d31edbf9ddf00a6
luanamartins <lms7@cin.ufpe.br>
2015-06-20 10:56:33 -0300

    Class Manager

---
+++ ClientChat/Controllers/Manager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClientChat.Controllers
+{
+    class Manager
+    {
+        public void loginRequest(string login, string password)
+        {
+
+        }
+    }
+}
