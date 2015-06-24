8e6e095680f31293be2dd17eee016f558a71f51b
luanamartins <lms7@cin.ufpe.br>
2015-06-23 21:20:25 -0300

    Add logo image to Loginview

--- ClientChat/Views/LoginView.Designer.cs
+++ ClientChat/Views/LoginView.Designer.cs
@@ -34,12 +34,14 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.LoginButton = new System.Windows.Forms.Button();
             this.newUserAccountButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(113, 76);
+            this.label1.Location = new System.Drawing.Point(267, 76);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(33, 13);
             this.label1.TabIndex = 0;
@@ -48,7 +50,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(116, 116);
+            this.label2.Location = new System.Drawing.Point(267, 116);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 1;
@@ -56,23 +58,23 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(242, 68);
+            this.textBox1.Location = new System.Drawing.Point(350, 69);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(184, 20);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(242, 116);
+            this.textBox2.Location = new System.Drawing.Point(350, 109);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(184, 20);
             this.textBox2.TabIndex = 3;
             // 
             // LoginButton
             // 
-            this.LoginButton.Location = new System.Drawing.Point(242, 156);
+            this.LoginButton.Location = new System.Drawing.Point(270, 161);
             this.LoginButton.Name = "LoginButton";
-            this.LoginButton.Size = new System.Drawing.Size(184, 23);
+            this.LoginButton.Size = new System.Drawing.Size(128, 23);
             this.LoginButton.TabIndex = 4;
             this.LoginButton.Text = "Log in";
             this.LoginButton.UseVisualStyleBackColor = true;
@@ -80,19 +82,28 @@
             // 
             // newUserAccountButton
             // 
-            this.newUserAccountButton.Location = new System.Drawing.Point(242, 197);
+            this.newUserAccountButton.Location = new System.Drawing.Point(404, 161);
             this.newUserAccountButton.Name = "newUserAccountButton";
-            this.newUserAccountButton.Size = new System.Drawing.Size(184, 23);
+            this.newUserAccountButton.Size = new System.Drawing.Size(128, 23);
             this.newUserAccountButton.TabIndex = 5;
             this.newUserAccountButton.Text = "New Account";
             this.newUserAccountButton.UseVisualStyleBackColor = true;
             this.newUserAccountButton.Click += new System.EventHandler(this.newUserAccountButton_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(33, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(160, 142);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
             // LoginView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(610, 295);
+            this.ClientSize = new System.Drawing.Size(606, 260);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.newUserAccountButton);
             this.Controls.Add(this.LoginButton);
             this.Controls.Add(this.textBox2);
@@ -101,6 +112,7 @@
             this.Controls.Add(this.label1);
             this.Name = "LoginView";
             this.Text = "ClientChat";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -114,6 +126,7 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button LoginButton;
         private System.Windows.Forms.Button newUserAccountButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
